ada6f888b4f8ce84c926f7b7818997084ee071d4
OwnerCO <jazminqntro96@gmail.com>
2025-01-28 23:01:23 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,10 +9,10 @@ const chalk = require('chalk');
 
 //~~~~~~~~~~~~< GLOBAL SETTINGS >~~~~~~~~~~~~\\
 
-global.owner = ['6282113821188'] //['6285174340858','6281356484612','6282113821188']
+global.owner = ['51960285930'] //['6285174340858','6281356484612','6282113821188']
 global.packname = 'Bot WhatsApp'
-global.author = 'Nazedev'
-global.botname = 'Naze Bot'
+global.author = 'CyberPsycho'
+global.botname = 'Cyber Bot'
 global.listprefix = ['+','!','.']
 global.listv = ['•','●','■','✿','▲','➩','➢','➣','➤','✦','✧','△','❀','○','□','♤','♡','◇','♧','々','〆']
 global.tempatDB = 'database.json'
